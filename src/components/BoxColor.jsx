04aa31590d9f819d41cf68784babb14efde16638
elisa <bascongarcia@gmail.com>
2022-07-24 21:18:44 +0200

    Iteration 4 Done

---
+++ src/components/BoxColor.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function BoxColor (props) {
+    return (
+        <div style={{backgroundColor: `rgb(${props.r}, ${props.g}, ${props.b})` }}>aloha</div>
+    )
+
+} 
