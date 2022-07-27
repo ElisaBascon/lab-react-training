3e060abc52fae0cded2c9c384c4bf015fe2f854f
elisa <bascongarcia@gmail.com>
2022-07-27 13:57:54 +0200

    Iteration 5 Style WIP

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function BoxColor (props) {
     return (
-        <div className="greetingCardStyle" style={{backgroundColor: `rgb(${props.r}, ${props.g}, ${props.b})` }}>
-            <div className="greetingCardInfo" >rgb({props.r}, {props.g}, {props.b})</div>
+        <div className="boxColorStyle" style={{backgroundColor: `rgb(${props.r}, ${props.g}, ${props.b})` }}>
+            <div className="boxColorInfo" >rgb({props.r}, {props.g}, {props.b})</div>
         </div>
     )
 
--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -3,16 +3,15 @@ import React from "react";
 export default function CreditCard(props) {
 
     return (
-        <div className="cardContainer">
             <div className="boxCreditCard">
                 <div className="creditCard " style={{backgroundColor: `${props.bgColor}` }}>
                     <img src={props.type} alt={props.type} />
-                    <h2>···· ···· ···· {props.number.slice(-4)}</h2>
-                    <h3>{props.expirationMonth} / {props.expirationYear}</h3>   
-                    <p>{props.bank}</p>
+                    <h2><strong>···· ···· ····</strong> {props.number.slice(-4)}</h2>
+                    <div>
+                    <h3>{props.expirationMonth} / {props.expirationYear}</h3><p>{props.bank}</p>
+                    </div>
                     <p>{props.owner}</p>
                 </div>
             </div>
-        </div>
     )
 }
