14d2d21d59d00c274400b80082d0dc39e788bc25
elisa <bascongarcia@gmail.com>
2022-08-14 18:10:56 +0200

    Iteration 14 in progress

--- src/components/FaceBook.jsx
+++ src/components/FaceBook.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Profiles from '../data/berlin.json';
 
 export default function FaceBook () {
 
+    const [color, setColor] = useState('white')
+    
+    const colorStyle = {
+        backgroundColor: `${color}`
+    }
+
     return (
         <div >
+        <button onClick={(contact) => {
+            contact.country === 'USA' ? setColor('blue') : setColor('white')
+        }}>USA</button>
+        <button onClick={() => setColor(color)}>England</button>
+        <button onClick={() => setColor(color)}>Malaysia</button>
+        <button onClick={() => setColor(color)}>Germany</button>
+        <button onClick={() => setColor(color)}>Sweden</button>
+
         {Profiles.map ((contact) => {
             return (
-                <div>
+                <div style={colorStyle}>
                     <div className="idCardStyle" >
                         <img src={contact.img} alt="profilePicture" />
                         <div className="idCardInfo" >
